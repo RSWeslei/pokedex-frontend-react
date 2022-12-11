215cda6ed8716b37e5eeb051a0fb85936d99588b
RSWeslei <weslei67rosiak.com@gmail.com>
2022-12-11 20:35:49 -0300

    add fetch

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,37 +1,48 @@
 import { Link } from '@react-navigation/native';
+import { KeyboardAvoidingView, Pressable, Text, TextInput, View } from 'react-native';
 import React from 'react';
 import { useState } from 'react';
-import { KeyboardAvoidingView, Pressable, Text, TextInput, View } from 'react-native';
-import { SafeAreaView } from 'react-native';
-import api from '../../plugins/axios';
+
+import apiFetch from '../../plugins/fetch'
 import { Styles } from './style';
 
 const SignUp = ({ navigation }) => {
-    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const validateSignUp = () => {
-        if(name !== '' && email !== '' && password !== '') {
-            api.post('/signup', { username: name, email: email, password: password })
-            .then((response) => {
-                console.log(response);
-            }).catch((error) => {
-                console.log(error);
-            })
-            navigation.navigate('Login');
+    const validateSignUp = async () => {
+        try {
+            if (!username || !email || !password) {
+                return alert('Please fill all fields');
+            }
+            let data = { username: username, email: email, password: password }
+            const options = {
+                method: 'POST',
+                body: JSON.stringify(data),
+            }
+            const response = await apiFetch('/signup', options);
+            const json = await response.json();
+            if (json.type === 'error' && response.status != 200) {
+                return alert(json.message);
+            }
+            if (json.type === 'success' && response.status == 200) {
+                return navigation.navigate('Login');
+            }
+        } catch (error) {
+            console.error(error);
         }
     }
 
     return (
-        <KeyboardAvoidingView style={Styles.LayoutStyle}>
-            <Link to={{ screen: 'Login' }} style={Styles.LinkRetorno}>{'< Login'}</Link>
-            <Text style={Styles.Title}>Cadastro Pokedéx</Text>
-            <TextInput style={Styles.InputLogin} onChangeText={text => setName(text)} placeholder="Nome" clearTextOnFocus />
-            <TextInput style={Styles.InputLogin} onChangeText={text => setEmail(text)} placeholder="E-mail" clearTextOnFocus />
-            <TextInput style={Styles.InputLogin} onChangeText={text => setPassword(text)} placeholder="Senha" clearTextOnFocus />
-            <Pressable onPress={validateSignUp} style={Styles.BotaoPadrao}>
-                <Text style={Styles.TextoBotao}>Cadastrar</Text>
+        <KeyboardAvoidingView style={Styles.layoutStyle}>
+            <Link to={{ screen: 'Login' }} style={Styles.returnLink}>{'< Login'}</Link>
+            <Text style={Styles.title}>Pokedex Signup</Text>
+            <TextInput style={Styles.inputLogin} onChangeText={text => setUsername(text)} placeholder="Username" clearTextOnFocus />
+            <TextInput style={Styles.inputLogin} onChangeText={text => setEmail(text)} placeholder="E-mail" clearTextOnFocus />
+            <TextInput style={Styles.inputLogin} onChangeText={text => setPassword(text)} placeholder="Password" clearTextOnFocus secureTextEntry={true}/>
+            <Pressable onPress={validateSignUp} style={Styles.defaultButton}>
+                <Text style={Styles.buttonText}>Signup</Text>
             </Pressable>
         </KeyboardAvoidingView>
     );
---
+++ src/plugins/fetch.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch';
+
+const apiFetch = async (url, options) => {
+    options.headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
+    return await fetch(`http://54.232.172.233${url}`, options)
+};
+
+export default apiFetch;
